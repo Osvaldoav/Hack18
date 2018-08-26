d6e330ca063fc683b4b06cf62ab8abd86da0c8c7
Osvaldoav <jorgeosvaldoav@gmail.com>
2018-08-26 11:39:18 -0500

    Casi listo

--- userapp/assets/containers/QRScreen.js
+++ userapp/assets/containers/QRScreen.js
@@ -18,7 +18,7 @@ class QRScreen extends Component {
     render(){
         const {product} = this.props;
         const {wachu} = this.props;
-        const {selectProduct, selectPedido} = this.props;
+        const {selectProduct, selectPedido, selectFooter} = this.props;
         return (
             <View style={styles.ContainerStyle}>
                 <Text style={{marginBottom: 5}}>Especificaciones de tu pedido:</Text>
@@ -47,7 +47,7 @@ class QRScreen extends Component {
                                 db.settings(settings);
                                 db.collection("ordenesPendientes").doc("id_0").set(data).then(()=>{
                                     console.log('listo');
-                                    Alert.alert('Pedido #231 confirmado!', 'Tu orden estara lista pronto', [{text: 'OK', onPress: () => console.log('OK Pressed')}], { cancelable: false });
+                                    Alert.alert('Pedido #231 confirmado!', 'Tu orden estara lista pronto', [{text: 'OK', onPress: () => {selectFooter('calendar'); Actions.Pedidos()}}], { cancelable: false });
                                     selectPedido(true);
                                 });
                                 wachu.refs.modal3.close();
