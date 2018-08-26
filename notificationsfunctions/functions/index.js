bfcdda4b1f49e28d0baf4213c549545d95993528
Jose Uvalle <marianok300@gmail.com>
2018-08-26 05:50:30 -0500

    Added notification function

--- notificationsfunctions/functions/index.js
+++ notificationsfunctions/functions/index.js
@@ -1,8 +1,33 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+var fetch = require('node-fetch')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+const messages = [{
+    to: 'ExponentPushToken[Xs4pdSKOhQw2JeSmkQvGwN]',
+    sound: 'default',
+    body: 'This comes from firebase',
+}];
+
+admin.initializeApp();
+
+exports.createOrder = functions.firestore
+    .document('ordenesPendientes/{orderId}')
+    .onCreate((snap, context) => {
+      // Get an object representing the document
+      // e.g. {'name': 'Marie', 'age': 66}
+      const newValue = snap.data();
+
+      // access a particular field as you would any JS property
+      const name = newValue.id;
+
+      fetch('https://exp.host/--/api/v2/push/send', {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(messages)
+      });
+    
+    });
